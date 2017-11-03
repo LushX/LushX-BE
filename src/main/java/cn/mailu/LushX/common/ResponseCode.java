4d5c5ef923ec2a37b189e36f8bd31df68dc9d0be
mmall <327137362@qq.com>
2017-11-03 23:59:47 +0800

    add response class

---
+++ src/main/java/cn/mailu/LushX/common/ResponseCode.java
@@ -0,0 +1,29 @@
+package cn.mailu.LushX.common;
+
+/**
+ * Created by null on 2017/11/3.
+ */
+
+public enum ResponseCode {
+    SUCCESS(0,"SUCCESS"),   //成功
+    ERROR(1,"ERROR"),       //失败
+    NEED_LOGIN(10,"NEED_LOGIN"),   //未登录
+    ILLEGAL_ARGUMENT(2,"ILLEGAL_ARGUMENT");   //非法参数
+
+    private final int code;
+    private final String desc;
+
+    ResponseCode(int code,String desc){
+        this.code=code;
+        this.desc=desc;
+    }
+
+    public int getCode() {
+
+        return code;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+}
