f0ab61b1815756478f778ab9925d233766481c9a
mmall <327137362@qq.com>
2017-11-03 20:24:37 +0800

    initialize project

---
+++ src/main/java/cn/mailu/LushX/LushXApplication.java
@@ -0,0 +1,12 @@
+package cn.mailu.LushX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LushXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LushXApplication.class, args);
+	}
+}
