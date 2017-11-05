76197c5332880a7114b2966a77e59c0dcbfafda9
zhenyaxu <zhenya_xu@163.com>
2017-11-05 20:45:06 +0800

    init repository

---
+++ src/main/java/cn/mailu/LushX/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package cn.mailu.LushX.repository;
+
+import cn.mailu.LushX.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @Ahtuor: xuzhenya
+ * @Description:
+ * @Date: Created in 下午 8:34 2017-11-05
+ * @Modified By:
+ */
+public interface UserRepository extends JpaRepository<User, String> {
+
+}
---
+++ src/main/java/cn/mailu/LushX/repository/VideoRepository.java
@@ -0,0 +1,13 @@
+package cn.mailu.LushX.repository;
+
+import cn.mailu.LushX.entity.Video;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @Ahtuor: xuzhenya
+ * @Description:
+ * @Date: Created in 下午 8:40 2017-11-05
+ * @Modified By:
+ */
+public interface VideoRepository extends JpaRepository<Video,String>{
+}
