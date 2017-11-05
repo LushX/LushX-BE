9a41bd36d5cbcd3e62e0eb43f714b43e2db6a2a0
benull <327137362@qq.com>
2017-11-05 20:09:02 +0800

    add controller

---
+++ src/main/java/cn/mailu/LushX/controller/backend/UserManageController.java
@@ -0,0 +1,16 @@
+package cn.mailu.LushX.controller.backend;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @Author: NULL
+ * @Description:
+ * @Date: Create in 2017/11/5 19:56
+ */
+@RestController
+@RequestMapping("/manage/user")
+@EnableSwagger2
+public class UserManageController {
+}
