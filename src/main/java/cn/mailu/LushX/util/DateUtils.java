6d3fb97bfe761f16c397210c5cdf83e889670738
zhenyaxu <zhenya_xu@163.com>
2017-11-06 09:07:01 +0800

    add DateUtils

---
+++ src/main/java/cn/mailu/LushX/util/DateUtils.java
@@ -0,0 +1,29 @@
+package cn.mailu.LushX.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+ /**
+ * @Ahtuor: xuzhenya
+ * @Description: 日期处理
+ * @Date: Created in 下午 8:43 2017-11-05
+ * @Modified By:
+ */
+public class DateUtils {
+	/** 时间格式(yyyy-MM-dd) */
+	public final static String DATE_PATTERN = "yyyy-MM-dd";
+	/** 时间格式(yyyy-MM-dd HH:mm:ss) */
+	public final static String DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
+	
+	public static String format(Date date) {
+        return format(date, DATE_PATTERN);
+    }
+
+    public static String format(Date date, String pattern) {
+        if(date != null){
+            SimpleDateFormat df = new SimpleDateFormat(pattern);
+            return df.format(date);
+        }
+        return null;
+    }
+}
