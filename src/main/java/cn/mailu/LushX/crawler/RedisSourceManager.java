4c03835ec9311f9758b99f28feabe2a09efa7a97
zhenyaxu <zhenya_xu@163.com>
2017-11-08 21:58:56 +0800

    Updating RedisSourceManager

--- src/main/java/cn/mailu/LushX/crawler/RedisSourceManager.java
+++ src/main/java/cn/mailu/LushX/crawler/RedisSourceManager.java
@@ -1,6 +1,7 @@
 package cn.mailu.LushX.crawler;
 
 
+import cn.mailu.LushX.entity.Article;
 import cn.mailu.LushX.entity.Video;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -16,46 +17,75 @@ import java.util.List;
 @Component
 public class RedisSourceManager {
 
-  public final String VIDEO_PREFIX_HOME_CAROUSEL_KEY = "HOME_VIDEO_CAROUSEL";
-  public final String VIDEO_PREFIX_HOME_RECOMMEND_KEY = "HOME_VIDEO_RECOMMEND";
-  public final String VIDEO_PREFIX_HOME_TV_KEY = "HOME_VIDEO_TV";
-  public final String VIDEO_PREFIX_HOME_MOVIE_KEY = "HOME_VIDEO_MOVIE";
-  public final String VIDEO_PREFIX_HOME_CARTOON_KEY = "HOME_VIDEO_CARTOON";
-  public final String VIDEO_PREFIx_HOME_LIVE_KEY = "HOME_LIVE";
-  public final String VIDEOS_KEY = "VIDEOS";
+    public final String VIDEO_PREFIX_HOME_CAROUSEL_KEY = "HOME_VIDEO_CAROUSEL";
+    public final String VIDEO_PREFIX_HOME_RECOMMEND_KEY = "HOME_VIDEO_RECOMMEND";
+    public final String VIDEO_PREFIX_HOME_TV_KEY = "HOME_VIDEO_TV";
+    public final String VIDEO_PREFIX_HOME_MOVIE_KEY = "HOME_VIDEO_MOVIE";
+    public final String VIDEO_PREFIX_HOME_CARTOON_KEY = "HOME_VIDEO_CARTOON";
+    public final String VIDEO_PREFIx_HOME_LIVE_KEY = "HOME_LIVE";
+    public final String JIANSHU_TRENDING_KEY = "JIANSHU_TRENDING";
+    public final String VIDEOS_KEY = "VIDEOS";
 
-  private final StringRedisTemplate stringRedisTemplate;
+    private final StringRedisTemplate stringRedisTemplate;
 
-  public RedisSourceManager(StringRedisTemplate stringRedisTemplate) {
-    this.stringRedisTemplate = stringRedisTemplate;
-  }
-
-  /**
-   *  保存视频信息到 Redis
-   */
-  void saveVideos(String key, List<Video> videos){
-    ObjectMapper mapper = new ObjectMapper();
-    try {
-      String value=mapper.writeValueAsString(videos);
-      stringRedisTemplate.opsForValue().set(key, value);
-    } catch (JsonProcessingException e) {
-      e.printStackTrace();
+    public RedisSourceManager(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
     }
-  }
 
-  /**
-   *  得到视频信息
-   */
-  public List<Video> getVideosByKeyAndTag(String key, String tag){
-    ObjectMapper mapper = new ObjectMapper();
-    key = key + "_" + tag;
-    String cacheValue = stringRedisTemplate.opsForValue().get(key);
-    try {
-      return mapper.readValue(cacheValue,mapper.getTypeFactory().constructParametricType(List.class,Video.class));
-    } catch (IOException e) {
-      e.printStackTrace();
+    /**
+     * 保存视频信息到 Redis
+     */
+    void saveVideos(String key, List<Video> videos) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            String value = mapper.writeValueAsString(videos);
+            stringRedisTemplate.opsForValue().set(key, value);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
     }
-    return null;
-  }
+
+    /**
+     * 得到视频信息
+     */
+    public List<Video> getVideosByKeyAndTag(String key, String tag) {
+        ObjectMapper mapper = new ObjectMapper();
+        key = key + "_" + tag;
+        String cacheValue = stringRedisTemplate.opsForValue().get(key);
+        try {
+            return mapper.readValue(cacheValue, mapper.getTypeFactory().constructParametricType(List.class, Video.class));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 保存简书信息到 Redis
+     */
+    void saveArticle(String key, List<Article> articles) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            String value = mapper.writeValueAsString(articles);
+            stringRedisTemplate.opsForValue().set(key, value);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 得到简书信息
+     */
+    public List<Article> getArticlesByKeyAndTag(String key, String tag) {
+        ObjectMapper mapper = new ObjectMapper();
+        key = key + "_" + tag;
+        String cacheValue = stringRedisTemplate.opsForValue().get(key);
+        try {
+            return mapper.readValue(cacheValue, mapper.getTypeFactory().constructParametricType(List.class, Article.class));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 }
