c73461cf401ee702efd8888f6e20482dc4cd30c9
zhenyaxu <zhenya_xu@163.com>
2017-11-17 13:49:37 +0800

    📝 Updating

--- src/main/java/cn/mailu/LushX/crawler/PandaCrawler.java
+++ src/main/java/cn/mailu/LushX/crawler/PandaCrawler.java
@@ -13,7 +13,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
