d9f413173be3c14616c3134e18858909bbeb1b4a
zhenyaxu <zhenya_xu@163.com>
2017-11-17 13:59:46 +0800

    📝 Updating MeiTuanCommentCrawler

---
+++ src/main/java/cn/mailu/LushX/crawler/MeituanCommentCrawler.java
@@ -0,0 +1,10 @@
+package cn.mailu.LushX.crawler;
+
+/**
+ * @Ahtuor: xuzhenya
+ * @Description:
+ * @Date: Created in 下午 3:17 2017-11-13
+ * @Modified By:
+ */
+public class MeituanCommentCrawler {
+}
