13a361fbc30cfba23a6cad24d71b423bebcb2d75
zhenyaxu <zhenya_xu@163.com>
2017-11-19 19:23:36 +0800

    📝 Adding C[o

--- src/main/java/cn/mailu/LushX/constant/RedisKey.java
+++ src/main/java/cn/mailu/LushX/constant/RedisKey.java
@@ -13,8 +13,9 @@ public class RedisKey {
     public static final String VIDEO_PREFIX_HOME_MOVIE_KEY = "HOME_VIDEO_MOVIE";
     public static final String VIDEO_PREFIX_HOME_CARTOON_KEY = "HOME_VIDEO_CARTOON";
     public static final String VIDEO_PREFIx_HOME_LIVE_KEY = "HOME_LIVE";
+    public static final String JIANSHU_NEW_KEY = "JIANSHU_NEW";
     public static final String JIANSHU_TRENDING_KEY = "JIANSHU_TRENDING";
     public static final String VIDEOS_KEY = "VIDEOS";
-    public static final String[] TAGS = {"QQ", "PANDA","Jianshu","YOUKU","IQIYI"};
+    public static final String[] TAGS = {"QQ", "PANDA","JIANSHU","YOUKU","IQIYI"};
 
 }
