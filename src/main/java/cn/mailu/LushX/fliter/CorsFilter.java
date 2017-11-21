304c456d6b9e805f9a26bed70fa87d96d42cfbc7
benull <327137362@qq.com>
2017-11-21 09:40:22 +0800

    :memo: update cors

--- src/main/java/cn/mailu/LushX/fliter/CorsFilter.java
+++ src/main/java/cn/mailu/LushX/fliter/CorsFilter.java
@@ -35,7 +35,7 @@ public class CorsFilter implements Filter {
         HttpServletResponse response= (HttpServletResponse) servletResponse;
         response.setHeader("Access-Control-Allow-Origin",request.getHeader("origin"));
         response.setHeader("Access-Control-Allow-Origin","*");  //允许跨域访问的域
-        response.setHeader("Access-Control-Allow-Methods","POST,GET,OPTIONS,DELETE");  //允许使用的请求方法
+        response.setHeader("Access-Control-Allow-Methods","POST,GET,OPTIONS,DELETE,PUT");  //允许使用的请求方法
         response.setHeader("Access-Control-Expose-Headers","*");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with,Cache-Control,Pragma,Content-Type,Authorization");  //允许使用的请求方法
         response.setHeader("Access-Control-Allow-Credentials","true");//是否允许请求带有验证信息
